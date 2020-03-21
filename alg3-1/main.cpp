498c4c5526ec7f690bf7750cacfaffd925b75049
kBeliczynski <kamcio1975@gmail.com>
2020-03-21 18:26:27 +0100

    this working

--- alg3-1/main.cpp
+++ alg3-1/main.cpp
@@ -2,31 +2,21 @@
 #include <algorithm>
 #include <vector>
 #include <limits.h>
-#include <queue>
 #include <cmath>
 using namespace std;
-
 struct Point{
     int x,y; // point coordinates
     int w=INT_MAX;  // weight
     int rest; //unit that show  the amount of stress reduction
     int index;
-    bool visit=false;
 };
 struct CompareWeight {
     bool operator()(Point * p1, Point * p2){
         return p1->w > p2->w;
     }
 };
-bool sortByX_withRest(Point *a, Point *b) {return (a->x+a->rest) < (b->x+b->rest);}
-bool sortByY_withRest(Point *a, Point *b) {return (a->y+a->rest) < (b->y+b->rest);}
-
-void print(Point **tab, int n){
-    for(int i=0; i<n; i++){
-        cout << "(" << tab[i]->x << "," << tab[i]->y << ") index: " << tab[i]-> index << endl;
-    }
-}
-
+bool sortByX_withRest(Point *a, Point *b) {return (a->x) < (b->x-b->rest);}
+bool sortByY_withRest(Point *a, Point *b) {return (a->y) < (b->y-b->rest);}
 void addNeighbors(vector<Point *> list[], int n, Point ** X, Point ** Y){ // add only nearest neighbors
     for(int i=0; i<n; i++){
         if(i==0){
@@ -50,29 +40,27 @@ int weight(Point a, Point *b){
  return (abs(a.x-b->x) < abs(a.y-b->y)) ? abs(a.x-b->x) : abs(a.y-b->y);
 }
 
-void dijkstra(vector<Point *> list[],int n,Point points[], int s, int d){
-        points[s].w=0;  // start
-     priority_queue<Point *,vector<Point*>,CompareWeight> Queue;
-       Queue.push(&points[s]);
-       while(!Queue.empty()){
-            if(Queue.top()->visit == false){
-                int ind = Queue.top()->index;
-                cout << "usunieto wartosc o indeksie : " << ind << endl;
-                Queue.pop();
-                for(int i=0; i<list[ind].size();i++){
-                    if(list[ind][i]->w > (points[ind].w + weight(points[ind],list[ind][i]))){
-                        list[ind][i]->w = (points[ind].w + weight(points[ind],list[ind][i]));
-                    }
-                    if(list[ind][i]->visit == false){
-                        cout << "dodano wartosc o indeksie : " << list[ind][i]->index << endl;
-                        Queue.push(list[ind][i]);
-                        }
-                }
-                points[ind].visit=true;
-            }
-            else
-                Queue.pop();
-       }
+void countDistance(vector<Point *> list[],int n, Point points[],int distance[][4]){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<list[i].size(); j++){
+            distance[i][j] = weight(points[i],list[i][j])-list[i][j]->rest;
+        }
+    }
+}
+
+bool Ford_Bellman(vector<Point *> list[],int n,Point points[], int s, int distance[][4]){
+    points[s].w=0; // start
+    for(int i=0; i<n-1; i++)
+        for(int j=0; j<n; j++)
+             for(int k=0; k<list[j].size(); k++){
+                if(points[j].w != INT_MAX && (list[j][k]->w > points[j].w+distance[j][k]))
+                    list[j][k]->w = points[j].w+distance[j][k];
+             }
+    for(int j=0; j<n; j++)
+             for(int k=0; k<list[j].size(); k++)
+                if(points[j].w != INT_MAX && (list[j][k]->w > points[j].w+distance[j][k]))
+                   return false;
+    return true;
 }
 
 int main(){
@@ -83,6 +71,7 @@ ios_base::sync_with_stdio(false);
     int v; // unit that show  the amount of stress reduction
         cin >> n;
         vector<Point *> neighborsList[n];
+        int distance[n][4];//distance from neighbor
         Point * points = new Point[n];  //main array with coordinates
         Point * pointX[n];              // only has pointers to a sorted array towards X
         Point * pointY[n];              // only has pointers to a sorted array towards Y
@@ -96,10 +85,11 @@ ios_base::sync_with_stdio(false);
     sort(pointX, pointX+n, sortByX_withRest);
     sort(pointY, pointY+n, sortByY_withRest);
     addNeighbors(neighborsList,n,pointX,pointY);
-    dijkstra(neighborsList,n,points,s,d);
-//for(int i=0; i<n; i++){
-//            cout << "(" << points[i].x << "," << points[i].y << ") rest: " << points[i].rest << " visit:" << points[i].visit << "the best path: " << points[i].w << endl;
-//        }
-    cout << points[d].w;
+    countDistance(neighborsList,n,points,distance);
+
+    if(Ford_Bellman(neighborsList,n,points,s,distance))
+        cout << points[d].w;
+    else
+        cout << "NIE ZA DOBRZE?";
     return 0;
 }
